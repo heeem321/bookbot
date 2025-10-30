e0d2f18c0afba9a76dcbf506813078ca0799ab74
ebrahim alhusaini <heeem321@hotmail.com>
2025-10-30 12:37:07 +0000

    Feature: take book path as CLI argument

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from stats import get_num_words, get_num_chars
+from stats import get_num_words, get_num_chars, get_sorted_chars
+import sys
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -7,10 +8,16 @@ def get_book_text(path_to_file):
     return file_contents
 
 def main():
-    book = get_book_text("./books/frankenstein.txt")
-    num_words = get_num_words(book)
-    print(f" Found {num_words} total words")
-    chars = get_num_chars(book)
-    print(chars)
+    if len(sys.argv) != 2:
+        print ("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        book = get_book_text(sys.argv[1])
+        num_words = get_num_words(book)
+        print(f" Found {num_words} total words")
+        chars = get_num_chars(book)
+        sorted_chars = get_sorted_chars(chars)
+        for item in sorted_chars:
+            if item["char"].isalpha(): print("{}: {}".format(item["char"], item["num"]))
 
 main()
--- stats.py
+++ stats.py
@@ -11,4 +11,16 @@ def get_num_chars(book):
 
     return chars
 
+def sort_on(items):
+    return items["num"]
+
+def get_sorted_chars(chars_dict):
+    sorted_dict = []
+    for k, v in chars_dict.items():
+        sorted_dict.append({"char": k, "num":v})
+
+    sorted_dict.sort(reverse=True, key=sort_on)
+
+    return sorted_dict
+
 
